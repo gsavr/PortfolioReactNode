5f89710f75e76ab37173529420adc329ea5cf6ab
giorgio <giorgiosavron@me.com>
2022-08-08 16:17:07 -0400

    initial commit

---
+++ client/src/components/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  console.log("Hello");
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+};
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT);
+console.log(`Listening on PORT:${PORT}`);
