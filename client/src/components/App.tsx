63344808f5683055daa789071a2f57dd118c58f6
giorgio <giorgiosavron@me.com>
2022-08-08 17:44:59 -0400

    redesigned server

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,11 @@
-export const App = () => {
-  console.log("Hello");
+import "../style/App.css";
+
+function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Portfolio</h1>
     </div>
   );
-};
+}
+
+export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
-import "./style/index.css";
-import { App } from "./components/App";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
