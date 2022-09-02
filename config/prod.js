e3663599e3cc91e3bd152edf3775a52cabad0c80
giorgio <giorgiosavron@me.com>
2022-09-02 10:53:45 -0400

    changed email service

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 //this is production keys
 module.exports = {
   sendGridKey: process.env.SENDGRID_KEY,
+  serviceId: process.env.SERVICE_ID,
+  templateId: process.env.TEMPLATE_ID,
+  emailjsKey: process.env.EMAILJS_KEY,
 };
