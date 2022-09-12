9b349b280b35bb71f082001139f5a5f1e0c487da
giorgio <giorgiosavron@me.com>
2022-09-11 20:04:27 -0400

    scroll to top button

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,11 +3,13 @@ import { Contact } from "./contact";
 import { Footer } from "./footer";
 import { Hero } from "./hero/hero";
 import { Projects } from "./projects/projects";
+import { StickyMenu } from "./sticky-menu";
 
 export const App: React.FC = () => {
   return (
     <div>
       <Hero />
+      <StickyMenu />
       <About />
       <Projects />
       <Contact />
