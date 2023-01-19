f481ef060c4f528f7c8ea433ae7b095b5717c151
giorgio <giorgiosavron@me.com>
2023-01-19 15:47:38 -0500

    resume update

--- client/src/components/hero/links.ts
+++ client/src/components/hero/links.ts
@@ -5,7 +5,7 @@ export const links = [
 ];
 
 export const resumeLink = {
-  link: "https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:a1a5ea63-b020-4e0a-a3ce-756181c27de8",
+  link: "https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:0db4ed0a-a1ab-4c19-a850-d212f014edd8",
   label: "Resume",
   target: "_blank",
   rel: "noreferrer",
