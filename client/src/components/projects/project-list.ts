72f635266ad10b07ed02621d5ed1b69e6d5d1584
giorgio <giorgiosavron@me.com>
2023-06-01 14:33:16 -0400

    projects update

--- client/src/components/projects/project-list.ts
+++ client/src/components/projects/project-list.ts
@@ -1,147 +1,132 @@
-import emailDesk from "../../images/projects/desktop/email.jpg";
-import emailMob from "../../images/projects/mobile/email.jpg";
-import triviaDesk from "../../images/projects/desktop/trivia.jpg";
-import triviaMob from "../../images/projects/mobile/trivia.jpg";
-import texterDesk from "../../images/projects/desktop/texter.jpg";
-import texterMob from "../../images/projects/mobile/texter.jpg";
-import swDesk from "../../images/projects/desktop/sw.jpg";
-import swMob from "../../images/projects/mobile/sw.jpg";
-import codeDesk from "../../images/projects/desktop/code.jpg";
-import codeMob from "../../images/projects/mobile/code.jpg";
-import hangDesk from "../../images/projects/desktop/hangman.jpg";
-import hangMob from "../../images/projects/mobile/hangman.jpg";
-import famguyDesk from "../../images/projects/desktop/famguy.jpg";
-import famguyMob from "../../images/projects/mobile/famguy.jpg";
-import moviesDesk from "../../images/projects/desktop/movies.jpg";
-import moviesMob from "../../images/projects/mobile/movies.jpg";
-import trackerDesk from "../../images/projects/desktop/tracker.jpg";
-import trackerMob from "../../images/projects/mobile/tracker.jpg";
-import foodieDesk from "../../images/projects/desktop/foodie.jpg";
-import foodieMob from "../../images/projects/mobile/foodie.jpg";
-import blogAiDesk from "../../images/projects/desktop/blog-ai.jpg";
-import blogAiMob from "../../images/projects/mobile/blog-ai.jpg";
-import fibAppDesk from "../../images/projects/desktop/fibapp.jpg";
-import fibAppMob from "../../images/projects/mobile/fibapp.jpg";
-import gatsbyDesk from "../../images/projects/desktop/gatsby.jpg";
-import gatsbyMob from "../../images/projects/mobile/gatsby.jpg";
-import owlzDesk from "../../images/projects/desktop/owlz.jpg";
-import owlzMob from "../../images/projects/mobile/owlz.jpg";
-import rpgDesk from "../../images/projects/desktop/rpg.jpg";
-import rpgMob from "../../images/projects/mobile/rpg.jpg";
+import { img } from "./projects-img";
 
 export const projects1 = [
   {
+    link: "https://chatty-ai-kappa.vercel.app/",
+    srcDesk: img.chatAiDesk,
+    srcMobile: img.chatAiMob,
+    title: "Chatty AI",
+    desc: "Next.js | OpenAI API | MongoDB | Auth-0", //
+  },
+  {
+    link: "https://book-management-system.herokuapp.com/",
+    srcDesk: img.bookMgrDesk,
+    srcMobile: img.bookMgrMob,
+    title: "Book Mgmt System",
+    desc: "React | Node.js | Apollo GraphQL | Prisma",
+  },
+  {
     link: "https://master.d3h3t6b4nsoxri.amplifyapp.com/",
-    srcDesk: blogAiDesk,
-    srcMobile: blogAiMob,
+    srcDesk: img.blogAiDesk,
+    srcMobile: img.blogAiMob,
     title: "Blogger Open-AI",
     desc: "Next.js | OpenAI API | MongoDB | Auth-0", //
   },
   {
     link: "https://emailman.herokuapp.com/",
-    srcDesk: emailDesk,
-    srcMobile: emailMob,
+    srcDesk: img.emailDesk,
+    srcMobile: img.emailMob,
     title: "Emailer",
     desc: "React | Node.js | Redux | GoogleAuth",
   },
   {
     link: "https://movie-search-gsavr.vercel.app/",
-    srcDesk: moviesDesk,
-    srcMobile: moviesMob,
+    srcDesk: img.moviesDesk,
+    srcMobile: img.moviesMob,
     title: "Movie Search",
     desc: "React | Redux Toolkit | Typescript",
   },
+];
+
+export const projects2 = [
   {
     link: "https://trivia-challenge-react.netlify.app/",
-    srcDesk: triviaDesk,
-    srcMobile: triviaMob,
+    srcDesk: img.triviaDesk,
+    srcMobile: img.triviaMob,
     title: "Trivia Challenge",
     desc: "React | ReduxToolkit | React-Router",
   },
 
   {
     link: "https://github.com/gsavr/activity-tracker-react-native",
-    srcDesk: trackerDesk,
-    srcMobile: trackerMob,
+    srcDesk: img.trackerDesk,
+    srcMobile: img.trackerMob,
     title: "Activity Tracker",
     desc: "React Native | Node.js | MongoDB",
   },
-];
-
-export const projects2 = [
   {
     link: "https://texter123-dev.herokuapp.com/",
-    srcDesk: texterDesk,
-    srcMobile: texterMob,
+    srcDesk: img.texterDesk,
+    srcMobile: img.texterMob,
     title: "Campaign Texting",
     desc: "React | Node.js | Twilio | Auth0",
   },
   {
     link: "https://github.com/gsavr/Code-Note-Editor",
-    srcDesk: codeDesk,
-    srcMobile: codeMob,
+    srcDesk: img.codeDesk,
+    srcMobile: img.codeMob,
     title: "Code & Text Editor",
     desc: "Run on Node env | npx jsnote-gs serve",
   },
   {
     link: "https://master.d3mg7hl4rh1qll.amplifyapp.com/",
-    srcDesk: fibAppDesk,
-    srcMobile: fibAppMob,
+    srcDesk: img.fibAppDesk,
+    srcMobile: img.fibAppMob,
     title: "Fibonacci Calculator",
     desc: "React | Docker | Node.js | AWS",
   },
+];
+
+export const projects3 = [
   {
     link: "https://api-sw-planets.netlify.app/",
-    srcDesk: swDesk,
-    srcMobile: swMob,
+    srcDesk: img.swDesk,
+    srcMobile: img.swMob,
     title: "API SW Planets",
     desc: "React | ReduxToolkit | React-Router",
   },
 
   {
     link: "https://owlz-app.herokuapp.com/",
-    srcDesk: owlzDesk,
-    srcMobile: owlzMob,
+    srcDesk: img.owlzDesk,
+    srcMobile: img.owlzMob,
     title: "Owlz App",
     desc: "React | Node.js | MySQL | ChatKit",
   },
-];
-
-export const projects3 = [
   {
     link: "https://github.com/gsavr/food-finder-react-native ",
-    srcDesk: foodieDesk,
-    srcMobile: foodieMob,
+    srcDesk: img.foodieDesk,
+    srcMobile: img.foodieMob,
     title: "Foodie Finder",
     desc: "React Native | Axios",
   },
-  {
+  /*  {
     link: "https://gatsbycontentfulcloudclass.gatsbyjs.io/",
-    srcDesk: gatsbyDesk,
-    srcMobile: gatsbyMob,
+    srcDesk: img.gatsbyDesk,
+    srcMobile: img.gatsbyMob,
     title: "Gatsby Rendered site",
     desc: "React | Gatsby | Contentful",
-  },
+  }, */
 
   {
     link: "https://gsavr.github.io/Word-Guess-Game/",
-    srcDesk: hangDesk,
-    srcMobile: hangMob,
+    srcDesk: img.hangDesk,
+    srcMobile: img.hangMob,
     title: "Science Hangman",
     desc: "Javascript | CSS",
   },
   {
     link: "https://gsavr.github.io/TriviaGame/ ",
-    srcDesk: famguyDesk,
-    srcMobile: famguyMob,
+    srcDesk: img.famguyDesk,
+    srcMobile: img.famguyMob,
     title: "Family Guy Trivia",
     desc: "JQuery | CSS",
   },
-  {
+  /* {
     link: "https://gsavr.github.io/sw-rpg/",
-    srcDesk: rpgDesk,
-    srcMobile: rpgMob,
+    srcDesk: img.rpgDesk,
+    srcMobile: img.rpgMob,
     title: "RPG Game",
     desc: "JQuery | CSS",
-  },
+  }, */
 ];
