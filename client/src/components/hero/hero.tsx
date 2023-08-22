f3241e845c2acb1256b509c07130c9f28424a0c8
giorgio <giorgiosavron@me.com>
2023-08-22 15:42:59 -0400

    parallax

--- client/src/components/hero/hero.tsx
+++ client/src/components/hero/hero.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { NavBar } from "./navBar";
 import { Link } from "react-scroll";
 
@@ -10,11 +10,18 @@ export const Hero: React.FC = () => {
     // offset - starts when start of element meets start of viewport - ends when end of element meets start of viewport
     offset: ["start start", "end start"],
   });
+  //add spring effect
+  const scrollYspring = useSpring(scrollYProgress, {
+    stiffness: 80,
+    damping: 20,
+    restDelta: 0.0001,
+  });
+
   //checks scroll - at value 0 we want to be at 0%, at value 1, we want to be at 100%
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   const backgroundFrontY = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
-  const navY = useTransform(scrollYProgress, [0, 0.05], ["0%", "-200%"]);
-  const boxesScale = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
+  const navY = useTransform(scrollYspring, [0, 0.05], ["0%", "-200%"]);
+  const boxesScale = useTransform(scrollYspring, [0, 1], ["100%", "0%"]);
   // const yRight = useTransform(scrollYProgress, [0, 1], ["0vh", "200vh"]);
   // const xRight = useTransform(scrollYProgress, [0, 1], ["0vw", "200vw"]);
   // const xLeft = useTransform(scrollYProgress, [0, 1], ["0vw", "-200vw"]);
